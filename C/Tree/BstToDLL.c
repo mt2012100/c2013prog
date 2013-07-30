9462f271695433ff718a4cf17f8b2de460904f98
pratibind.jha <pratibind.jha@f585daad-8472-54d6-6279-b4057fc9bb80>
2013-07-30 10:47:56 +0000

    bstDLL image

--- C/Tree/BstToDLL.c
+++ C/Tree/BstToDLL.c
@@ -120,11 +120,13 @@ int main() {
     Node root = NULL;
     Node head;
     
-    treeInsert(&root, 4);
-    treeInsert(&root, 2);
+    treeInsert(&root, 9);
+    treeInsert(&root, 7);
+    treeInsert(&root, 6);
+    treeInsert(&root, 8);
     treeInsert(&root, 1);
     treeInsert(&root, 3);
-    treeInsert(&root, 5);
+    treeInsert(&root, 15);
     
     head = treeToList(root);
     
