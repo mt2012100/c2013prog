0eaf5fa3384e7489143d5ad314b43b52dfd13bb9
pratibind.jha <pratibind.jha@f585daad-8472-54d6-6279-b4057fc9bb80>
2013-08-06 16:13:19 +0000

    initial version of pointer output file has been added.

---
+++ C/CQuest/MCQpointer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+
+/*int main() {
+    char *s = "Alice";
+    int len = strlen(s);
+    int i=0;
+    for(;i<= len;i++) {
+        
+        printf("%s ",s);
+        s++;
+        
+    }
+    
+    return 0;
+}*/
+
+//output: Alice lice ice ce c
