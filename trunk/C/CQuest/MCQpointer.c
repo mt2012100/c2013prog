a36cf5ccad877b15e63bd2591bd4ec1579e038f5
pratibind.jha <pratibind.jha@f585daad-8472-54d6-6279-b4057fc9bb80>
2013-08-06 19:15:18 +0000

    initial version of pointer output file has been added.

--- trunk/C/CQuest/MCQpointer.c
+++ trunk/C/CQuest/MCQpointer.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 
-
+//Question 1
 /*int main() {
     char *s = "Alice";
     int len = strlen(s);
@@ -17,3 +17,46 @@
 }*/
 
 //output: Alice lice ice ce c
+
+//Question 2.
+/*
+
+int main() {
+    char *s = "%d\n";
+    int i=9;
+    s++;
+    s++;
+    printf(s-2,i);
+    
+return 0;
+}
+*/
+
+//output: 9
+
+//Question 3
+
+/*
+int main() {
+    char *s = "%d\nRST";
+    int i=9;
+    s++;
+    s++;
+    printf(s);
+    
+return 0;
+}
+*/
+//output: RST
+
+//Question 4
+/*
+int main() {
+  char t='a';
+  printf("%d %d %d %d\n",sizeof(""),sizeof("A"),sizeof('a'),sizeof(t));
+  return 0;
+}
+*/
+//output: 1 2 4 1 reason: '' is treated as string so sizeof operator return pointer size. where as "" treated as array of character
+//including \0 so "A" return 2 byte. 
+
